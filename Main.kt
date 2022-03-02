d3ab96be3cf41f7cfcc37d1d32fc6c3f73f74d37
Yudawardana94 <yuda.wardana40@gmail.com>
2022-03-02 21:34:29 +0700

    change logic and project structure

--- Main.kt
+++ Main.kt
@@ -1,41 +1,70 @@
-fun main(args: Array<String>) {
-    println("===========================================")
-    println("Selamat datang di permainal ter-jan-ken-pow \n")
-    println("silahkan masukkan 'gunting', 'batu', atau 'kertas' untuk melanjutkan")
-    println("===========================================")
 
-    println("")
-    println("Masukkan pemain 1 :")
-    var player1 = readLine()
-    var player1Validation = player1 == "kertas" || player1 == "gunting" || player1 == "batu"
-    println("")
-    println("Masukkan pemain 2: ")
-    var player2 = readLine()
-    var player2Validation = player2 =="kertas" || player2 == "gunting" || player2 == "batu"
+fun main (args: Array<String>) {
+        println("===========================================")
+        println("Selamat datang di permainal ter-jan-ken-pow \n")
+        println("silahkan masukkan 'gunting', 'batu', atau 'kertas' untuk melanjutkan")
+        println("===========================================")
 
-    var isValid = player1Validation == true && player2Validation == true
+        // set player 1
+        println("\nMasukkan pemain 1 :")
+        var handSign1: String? = readLine()
+        val Player1 = Player("Player 1", handSign1?.trim() )
+        var isPlayer1Valid = Player1.validateInput(handSign1?.trim()!!)
 
-    if(isValid) {
-        var isSame = player1 == player2
-        var result = ""
-        if(player1 == "kertas" && player2 == "gunting") result = "Player 2"
-        else if(player1 == "kertas" && player2 == "batu") result = "Player 1"
-        else if(player1 == "gunting" && player2 == "batu") result = "Player 2"
-        else if(player1 == "gunting" && player2 == "kertas") result = "Player 1"
-        else if(player1 == "batu" && player2 == "kertas") result = "Player 2"
-        else if(player1 == "batu" && player2 == "gunting") result = "Player 1"
+        // set player2
+        println("\nMasukkan pemain 2 :")
+        var handSign2: String? = readLine()
+        val Player2 = Player("Player 2", handSign2?.trim() )
+        var isPlayer2Valid = Player2.validateInput(handSign2?.trim()!!)
 
-        println("player 1 is ${player1?.uppercase()} vs player2 is ${player2?.uppercase()} \n")
+        // make game class
+        val JanKenPo = Game()
+        var isGameValid = JanKenPo.isValid(isPlayer1Valid, isPlayer2Valid)
 
-        println("HASIL: ")
-        println("tunggu yaa~")
-        Thread.sleep(1000)
-        if(isSame) {
-            println("DRAW !!!")
-        } else if(result != "") {
-            println("${result.uppercase()} MENANG !!!")
+        if(isGameValid) {
+                println("mainkan gamenya")
+                var result =  JanKenPo.play(Player1!!, Player2!!);
+                if(result == "draw") {
+                        println("DRAW !!!")
+                } else {
+                        println("${result.uppercase()} MENANG !!!")
+                }
+        } else {
+                println("Kamu salah memasukkan pilihan, coba ulangi lagi ya :)")
         }
-    } else {
-        println("Kamu salah memasukkan pilihan, coba ulangi lagi ya :)")
-    }
+
+}
+
+class Player(playerName: String?, playerHand: String?) {
+    var playerName: String? = playerName
+    var playerHand: String? = playerHand
+        fun validateInput(PlayerHand: String): Boolean {
+                val options = arrayOf("gunting", "batu", "kertas")
+                var valid = false
+                for(option in options) {
+                        if (!valid && option == PlayerHand) { valid = true }
+                }
+                return valid
+        }
+}
+
+class Game {
+        fun play(Player1: Player, Player2: Player): String {
+                var result = "draw"
+
+                if(Player1.playerHand == "kertas" && Player2.playerHand == "gunting") result = Player2.playerName.toString()
+                else if(Player1.playerHand == "kertas" && Player2.playerHand == "batu") result = Player1.playerName.toString()
+                else if(Player1.playerHand == "gunting" && Player2.playerHand == "batu") result = Player2.playerName.toString()
+                else if(Player1.playerHand == "gunting" && Player2.playerHand == "kertas") result = Player1.playerName.toString()
+                else if(Player1.playerHand == "batu" && Player2.playerHand == "kertas") result = Player2.playerName.toString()
+                else if(Player1.playerHand == "batu" && Player2.playerHand == "gunting") result = Player1.playerName.toString()
+
+                return result;
+        }
+
+        fun isValid(player1Input: Boolean?, player2Input: Boolean?): Boolean {
+                println("$player1Input, $player2Input, yang ini ya bosquee")
+                if(player1Input == true && player2Input == true) return true;
+                return false;
+        }
 }
